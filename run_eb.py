a8ea2772734c096126a240c8f90c9a54d1a55b58
molpopgen <krthornt@uci.edu>
2019-08-29 02:25:04 -0700

    Add "one pass" test of buffering with overlapping generations.

--- run_eb.py
+++ run_eb.py
@@ -5,7 +5,18 @@ np.random.seed(333)
 
 pstate = wright_fisher.PopState(100)
 
-pstate = wright_fisher.wright_fisher(2000, 0.9, pstate)
+pstate = wright_fisher.wright_fisher_eb(2000, 0.9, pstate)
+
+for o in reversed(pstate.generation_offsets):
+    for i in range(*o):
+        for j in pstate.buffered_edges[i][0]:
+            pstate.tables.edges.add_row(*j)
+        for j in pstate.buffered_edges[i][1]:
+            pstate.tables.edges.add_row(*j)
+
+
+# for eb in pstate.buffered_edges:
+#     print(eb)
 
 flags = np.zeros(len(pstate.tables.nodes), dtype=np.uint32)
 for p in pstate.parents:
@@ -21,5 +32,5 @@ samples = np.where(flags == 1)[0]
 ts = pstate.tables.tree_sequence()
 node_colors = {idmap[i]: 'green' for i in samples}
 t = next(ts.trees())
-t.draw(path="tree.svg", format="svg", height=1000,
+t.draw(path="tree_eb.svg", format="svg", height=1000,
        width=1000, node_colours=node_colors)
--- wright_fisher.py
+++ wright_fisher.py
@@ -15,6 +15,8 @@ class PopState(object):
         self.parents = [Parent(i, 2*i, 2*i+1) for i in range(N)]
         self.next_parent = N
         self.tables = tskit.TableCollection(1.0)
+        self.buffered_edges = [[[], []] for i in range(N)]
+        self.generation_offsets = [(0, len(self.buffered_edges))]
 
         # Measure time going forwards.
         # Will reverse later
@@ -46,7 +48,7 @@ def wright_fisher(ngens, psurvival, popstate):
                     p1node = popstate.parents[parents[1]].n1
                 parent_list.append((p0node, p1node))
                 dead.append(i)
-        for d,p in zip(dead,parent_list):
+        for d, p in zip(dead, parent_list):
             n0 = popstate.tables.nodes.add_row(time=gen)
             n1 = popstate.tables.nodes.add_row(time=gen)
             popstate.tables.edges.add_row(
@@ -55,5 +57,55 @@ def wright_fisher(ngens, psurvival, popstate):
                 left=0, right=1, parent=p[1], child=n1)
             popstate.parents[d] = Parent(popstate.next_parent, n0, n1)
             popstate.next_parent += 1
+
+    return popstate
+
+
+def wright_fisher_eb(ngens, psurvival, popstate):
+    if psurvival >= 1. or psurvival < 0:
+        raise ValueError("unhelpful survival probability")
+
+    for gen in range(1, ngens+1):
+        # regulation
+        dead = []
+        parent_list = []
+        for i, p in enumerate(popstate.parents):
+            if p.index == -1:
+                raise RuntimeError("oops, dead!")
+            if np.random.uniform() > psurvival:
+                p.index = -1
+                parents = np.random.choice(len(popstate.parents), 2)
+                # "Mendel"
+                p0node = popstate.parents[parents[0]].n0
+                i0 = 0
+                if np.random.uniform() < 0.5:
+                    p0node = popstate.parents[parents[0]].n1
+                    i0 = 1
+                p1node = popstate.parents[parents[1]].n0
+                i1 = 0
+                if np.random.uniform() < 0.5:
+                    p1node = popstate.parents[parents[1]].n1
+                    i1 = 1
+                parent_list.append((p0node, p1node, i0, i1,
+                                    popstate.parents[parents[0]].index,
+                                    popstate.parents[parents[1]].index))
+                dead.append(i)
+        x = len(popstate.buffered_edges)
+        for d, p in zip(dead, parent_list):
+            n0 = popstate.tables.nodes.add_row(time=gen)
+            n1 = popstate.tables.nodes.add_row(time=gen)
+            # popstate.tables.edges.add_row(
+            #     left=0, right=1, parent=p[0], child=n0)
+            # popstate.tables.edges.add_row(
+            #     left=0, right=1, parent=p[1], child=n1)
+            popstate.buffered_edges[p[4]][p[2]].append((0, 1, p[0], n0))
+            popstate.buffered_edges[p[5]][p[3]].append((0, 1, p[1], n1))
+            popstate.parents[d] = Parent(popstate.next_parent, n0, n1)
+            popstate.next_parent += 1
+            popstate.buffered_edges.append([[], []])
+
+        if len(dead) > 0:
+            popstate.generation_offsets.append(
+                (x, len(popstate.buffered_edges)))
 
     return popstate
