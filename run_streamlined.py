d72d4107f0d58c3609647d4ddfd2a248b6ea64ce
molpopgen <krthornt@uci.edu>
2020-04-28 17:26:04 -0700

    Add runner for streamlined implementation.

---
+++ run_streamlined.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import streamlined_prototype
+
+tables, tables2 = streamlined_prototype.wright_fisher(500, 1000, 0.0, 33)
+assert np.array_equal(tables.nodes.time, tables2.nodes.time)
+ts = tables.tree_sequence()
+ts2 = tables2.tree_sequence()
+m = streamlined_prototype.pairwise_distance_branch(ts, [i for i in ts.samples()])
+m2 = streamlined_prototype.pairwise_distance_branch(ts2, [i for i in ts2.samples()])
+assert np.array_equal(m, m2)
+ts.first().draw(format="svg", path="foo.svg", height=5000, width=5000)
+ts.first().draw(format="svg", path="foo2.svg", height=5000, width=5000)
