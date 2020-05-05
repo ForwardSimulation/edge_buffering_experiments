c9af92997b75ef3a62b5bd49326757102163bfbd
molpopgen <krthornt@uci.edu>
2020-05-05 15:38:09 -0700

    Compiled tskit and add table_collection_ptr.

---
+++ tskit_tools.cc
@@ -0,0 +1,18 @@
+#include <stdexcept>
+#include "tskit_tools.hpp"
+
+table_collection_ptr
+make_table_collection_ptr(double sequence_length)
+{
+    table_collection_ptr rv(new tsk_table_collection_t(),
+                            [](tsk_table_collection_t* tables) {
+                                tsk_table_collection_free(tables);
+                                delete tables;
+                            });
+    int err = tsk_table_collection_init(rv.get(), sequence_length);
+    if (err != 0)
+        {
+            throw std::runtime_error("could not initialize tsk_table_collection");
+        }
+    return rv;
+}
--- wfbuffered.cc
+++ wfbuffered.cc
@@ -4,9 +4,11 @@
 #include <tskit.h>
 
 #include "rng.hpp"
+#include "tskit_tools.hpp"
 
 int
 main(int argc, char **argv)
 {
     auto rng = make_rng(42);
+    auto tables = make_table_collection_ptr(11.);
 }
