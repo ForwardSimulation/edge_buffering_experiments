fca876c18d63c61db27aabda5bdda2ce6629243a
molpopgen <krthornt@uci.edu>
2020-05-05 19:10:43 -0700

    First draft of "standard" method w/o recombination.

--- simulate.cc
+++ simulate.cc
@@ -1,3 +1,4 @@
+#include <bits/c++config.h>
 #include <vector>
 #include <gsl/gsl_randist.h>
 #include "rng.hpp"
@@ -5,12 +6,22 @@
 
 namespace
 {
-    struct IndexAndNodes
+    struct Parent
     {
         std::size_t index;
         tsk_id_t node0, node1;
-        IndexAndNodes(std::size_t i, tsk_id_t n0, tsk_id_t n1)
-            : index(i), node0(n0), node1(n1)
+        Parent(std::size_t i, tsk_id_t n0, tsk_id_t n1) : index(i), node0(n0), node1(n1)
+        {
+        }
+    };
+
+    struct Birth
+    {
+        std::size_t index;
+        tsk_id_t p0node0, p0node1, p1node0, p1node1;
+        Birth(std::size_t i, const Parent& p0, const Parent& p1)
+            : index(i), p0node0(p0.node0), p0node1(p0.node1), p1node0(p1.node0),
+              p1node1(p1.node1)
         {
         }
     };
@@ -30,18 +41,90 @@ record_node(double t, table_collection_ptr& tables)
 }
 
 void
-simulate(GSLrng& rng, table_collection_ptr& tables, unsigned N, double psurvival,
-         unsigned nsteps, unsigned simplification_interval)
+deaths_and_parents(const GSLrng& rng, const std::vector<Parent>& parents,
+                   double psurvival, std::vector<Birth>& births)
 {
-    std::vector<IndexAndNodes> parents;
-    for (unsigned i = 0; i < 2 * N; ++i)
+    births.clear();
+    for (std::size_t i = 0; i < parents.size(); ++i)
+        {
+            if (gsl_rng_uniform(rng.get()) > psurvival)
+                {
+                    std::size_t parent0 = gsl_ran_flat(rng.get(), 0, parents.size());
+                    std::size_t parent1 = gsl_ran_flat(rng.get(), 0, parents.size());
+                    births.emplace_back(i, parents[parent0], parents[parent1]);
+                }
+        }
+}
+
+void
+generate_births(const GSLrng& rng, const std::vector<Birth>& births, double birth_time,
+                table_collection_ptr& tables)
+{
+    for (auto& b : births)
+        {
+            auto new_node_0 = record_node(birth_time, tables);
+            auto new_node_1 = record_node(birth_time, tables);
+            auto parental_node0 = b.p0node0;
+            if (gsl_rng_uniform(rng.get()) < 0.5)
+                {
+                    parental_node0 = b.p0node1;
+                }
+            auto parental_node1 = b.p1node0;
+            if (gsl_rng_uniform(rng.get()) < 0.5)
+                {
+                    parental_node1 = b.p1node1;
+                }
+            auto rv = tsk_edge_table_add_row(&tables->edges, 0., tables->sequence_length,
+                                             parental_node0, new_node_0, nullptr, 0);
+            rv = tsk_edge_table_add_row(&tables->edges, 0., tables->sequence_length,
+                                        parental_node1, new_node_1, nullptr, 0);
+        }
+}
+
+void
+sort_n_simplify(std::vector<Parent>& parents, table_collection_ptr& tables)
+{
+    int rv = tsk_table_collection_sort(tables.get(), nullptr, 0);
+    std::vector<tsk_id_t> samples;
+    for (auto& p : parents)
+        {
+            samples.push_back(p.node0);
+            samples.push_back(p.node0);
+        }
+    rv = tsk_table_collection_simplify(tables.get(), samples.data(), samples.size(), 0,
+                                       nullptr);
+}
+
+void
+simulate(const GSLrng& rng, unsigned N, double psurvival, unsigned nsteps,
+         unsigned simplification_interval, table_collection_ptr& tables)
+{
+    std::vector<Parent> parents;
+    for (unsigned i = 0; i < N; ++i)
         {
             auto id0 = record_node(nsteps, tables);
             auto id1 = record_node(nsteps, tables);
             parents.emplace_back(i, id0, id1);
         }
 
-    for (unsigned step = 1; step < nsteps; ++step)
+    std::vector<Birth> births;
+    bool simplified = false;
+    for (unsigned step = 1; step <= nsteps; ++step)
         {
+            deaths_and_parents(rng, parents, psurvival, births);
+            generate_births(rng, births, nsteps - step, tables);
+            if (step % simplification_interval == 0.)
+                {
+                    sort_n_simplify(parents, tables);
+                    simplified = true;
+                }
+            else
+                {
+                    simplified = false;
+                }
+        }
+    if (simplified == false)
+        {
+            sort_n_simplify(parents, tables);
         }
 }
--- simulate.hpp
+++ simulate.hpp
@@ -3,5 +3,5 @@
 class GSLrng;
 class table_collection_ptr;
 
-void simulate(GSLrng& rng, table_collection_ptr& tables, unsigned N, double psurvival,
-              unsigned nsteps, unsigned simplification_interval);
+void simulate(const GSLrng& rng, unsigned N, double psurvival, unsigned nsteps,
+              unsigned simplification_interval, table_collection_ptr& tables);
